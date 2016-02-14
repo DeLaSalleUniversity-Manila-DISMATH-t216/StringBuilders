aab7adbaf39bf7dd388708f8b8ebd0cec8849b40
JC Bautista <JC Bautista>
2016-02-13 17:10:19 -0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "Watashi no namae wa";
+        StringBuilder sb = new StringBuilder(s1);
+
+        sb.append(" JC desu");
+
+        System.out.println(sb);
+    }
+}
